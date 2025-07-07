d0705b01c9674cc4772d4ea380751fd8cebc5766
franfreezy <frandelwanjawa@yahoo.com>
2025-07-07 21:30:14 +0300

    Fix main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client'; 
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { HashRouter } from 'react-router-dom';
 
-
 const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
 
-root.render(
-  <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </React.StrictMode>
-);
+if (rootElement) {
+  const root = createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </React.StrictMode>
+  );
+} else {
+  throw new Error("Root element with ID 'root' not found.");
+}
